0a63c4f77926403f5f200011162157aa5e568348
matxon <kmpda@inbox.ru>
2024-04-08 12:16:02 +0500

    card is toggleable

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻","🎃","🕷️","👿"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(content: emojis[0], isFace: true)
+            CardView(content: emojis[1])
+            CardView(content: emojis[2])
+            CardView(content: emojis[3])
         }
         .padding()
+        .foregroundColor(.orange)
     }
 }
+
+struct CardView: View {
+    let content: String
+    @State var isFace = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius:12)
+            
+            if isFace {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture(perform: {
+            isFace.toggle()
+        })
+    }
+}
+
+
+
+
 
 #Preview {
     ContentView()
